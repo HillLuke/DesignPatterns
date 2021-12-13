bef451b89c078d6f16c786d65b8e22f4468aa52a
HillLuke <lukehilldev@gmail.com>
2021-12-13 17:35:00 +0000

    Initial commit

---
+++ DesignPatterns_Console/Creational/AbstractFactory.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns_Console.Creational
+{
+    public class AbstractFactory : IDesignPattern
+    {
+        public void RunExample()
+        {
+            var DesertBiomeFactory = new DesertBiomeFactory();
+            var DesertBiome = DesertBiomeFactory.MakeBiome();
+            Console.WriteLine($"Biome type {DesertBiome.GetBiomeName()}");
+
+            var ForestBiomeFactory = new ForestBiomeFactory();
+            var ForestBiome = ForestBiomeFactory.MakeBiome();
+            Console.WriteLine($"Biome type {ForestBiome.GetBiomeName()}");
+        }
+    }
+
+    abstract class BiomeFactory 
+    { 
+        public abstract Biome MakeBiome();
+    }
+
+    class DesertBiomeFactory : BiomeFactory
+    {
+        public override Biome MakeBiome()
+        {
+            return new DesertBiome();
+        }
+    }
+    class ForestBiomeFactory : BiomeFactory
+    {
+        public override Biome MakeBiome()
+        {
+            return new ForestBiome();
+        }
+    }
+
+    abstract class Biome
+    {
+        public abstract string GetBiomeName();
+    }
+
+    class ForestBiome : Biome
+    {
+        public override string GetBiomeName()
+        {
+            return "Forest";
+        }
+    }
+    class DesertBiome : Biome
+    {
+        public override string GetBiomeName()
+        {
+            return "Desert";
+        }
+    }
+
+
+}
---
+++ DesignPatterns_Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using DesignPatterns_Console.Creational;
+using System;
+
+namespace DesignPatterns_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Example = new AbstractFactory();
+
+            Example.RunExample();
+        }
+    }
+}
